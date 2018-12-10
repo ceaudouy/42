302cad9a4d7f51953ae88e6dcb26fa201bf31f14
Cedric AUDOUY <ceaudouy@e2r6p6.42.fr>
2018-12-10 16:45:40 +0100

    checkerror add check_tetri

--- fillit/checkerror.c
+++ fillit/checkerror.c
@@ -6,7 +6,7 @@
 /*   By: ceaudouy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 12:13:07 by ceaudouy          #+#    #+#             */
-/*   Updated: 2018/12/10 16:34:30 by ceaudouy         ###   ########.fr       */
+/*   Updated: 2018/12/10 16:43:42 by ceaudouy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,44 @@ int		ft_checkerror(char *tab)
 	if ((ft_strlen(tab) == i && d == 4 && p == 12 && tab[i - 1] == '\n'))
 		return (0);
 	return (1);
+}
+
+static int	ft_check(char *tab, int i)
+{
+	if (tab[i + 1] == '#')
+		i++;
+	else if (tab[i + 3] == '#')
+		i = i + 3;
+	else if (tab[i + 4] == '#')
+		i = i + 4;
+	else if (tab[i + 5] == '#')
+		i = i + 5;
+	else
+		i = -1;
+	return (i);
+}
+
+int			ft_check_tetri(char *tab)
+{
+	int		i;
+	int		d;
+
+	i = 0;
+	d = 0;
+	while (tab[i])
+	{
+		if (tab[i] == '#')
+		{
+			d++;
+			if (d == 4)
+				if (tab[i - 1] == '#' || tab[i - 5] == '#')
+					return (0);
+			i = ft_check(tab, i);
+			if (i == -1)
+				return (1);
+		}
+		else
+			i++;
+	}
+	return (0);
 }
