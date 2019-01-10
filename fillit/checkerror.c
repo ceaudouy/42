541c37c4ab34c546c774650fdae98eeabc8b4765
Cedric AUDOUY <ceaudouy@e2r6p3.42.fr>
2019-01-10 17:16:22 +0100

    fillit

--- fillit/checkerror.c
+++ fillit/checkerror.c
@@ -6,7 +6,7 @@
 /*   By: ceaudouy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 12:13:07 by ceaudouy          #+#    #+#             */
-/*   Updated: 2019/01/09 13:04:42 by ceaudouy         ###   ########.fr       */
+/*   Updated: 2019/01/10 17:09:37 by ceaudouy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,44 +36,41 @@ int			ft_checkerror(char *tab)
 	return (1);
 }
 
-static int	ft_check(char *tab, int i)
+static int	ft_link(char *tab, int i)
 {
-	if (tab[i + 1] == '#')
-		i++;
-	else if (tab[i + 3] == '#')
-		i = i + 3;
-	else if (tab[i + 4] == '#')
-		i = i + 4;
-	else if (tab[i + 5] == '#')
-		i = i + 5;
-	else
-		i = -1;
-	return (i);
+	int		l;
+
+	l = 0;
+	if (i > 0 && tab[i - 1] == '#')
+		l++;
+	if (i < 20 && tab[i + 1] == '#')
+		l++;
+	if (i >= 5 && tab[i - 5] == '#')
+		l++;
+	if (i < 15 && tab[i + 5] == '#')
+		l++;
+	return (l);
 }
 
 int			ft_check_tetri(char *tab)
 {
 	int		i;
-	int		d;
+	int		l;
 
 	i = 0;
-	d = 0;
+	l = 0;
 	while (tab[i])
 	{
 		if (tab[i] == '#')
 		{
-			d++;
-			if (d == 4)
-				if (tab[i - 1] == '#' || tab[i - 5] == '#')
-					return (0);
-			i = ft_check(tab, i);
-			if (i == -1)
-				return (1);
+			l += ft_link(tab, i);
 		}
-		else
-			i++;
+		i++;
 	}
-	return (0);
+	if (l >= 6)
+		return (0);
+	else
+		return (1);
 }
 
 int			ft_check_end(char *tab)
